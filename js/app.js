1930a21abc80d271cbecc87eb2e16f57b89e5e7f
Jonathan <jona_play_music@hotmail.com>
2025-02-01 22:22:26 -0300

    agregando archivos js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+Dropzone.autoDiscover =false;
+
+const dropzone = new Dropzone('#dropzone',{
+    dictDefaultMessage: 'Sube aqui tu imagen',
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo'
+})
